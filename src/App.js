cbef200efe7df5702e4a69913b343f13b756711d
Thomas BENARD <thomas0benard@gmail.com>
2021-09-22 10:13:12 +0200

    sent fetched users to state

--- src/App.js
+++ src/App.js
@@ -6,27 +6,14 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-          id: "asze",
-        },
-        {
-          name: "Dracula",
-          id: "as32",
-        },
-        {
-          name: "Zombie",
-          id: "as49",
-        },
-      ],
+      monsters: [],
     };
   }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
-      response.json()
-    );
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
