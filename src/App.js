f2ac1b012b9b4d888dad1c1cd47e449262ca907d
Thomas BENARD <thomas0benard@gmail.com>
2021-09-27 10:01:47 +0200

    added  search filter on monster array

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
         <input
@@ -25,7 +29,7 @@ class App extends Component {
           placeholder="search monsters"
           onChange={(e) => this.setState({ searchField: e.target.value })}
         />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
